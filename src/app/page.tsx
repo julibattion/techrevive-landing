f945682359055fc2bcacb24fac8158285f38f994
Rodrigo Crespo <cresporodrigomanuel@gmail.com>
2024-04-10 21:45:39 -0300

    Add form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Example() {
 
   return (
     <div>
-      <header className="absolute inset-x-0 top-0 z-50">
+      <header>
         <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5">
@@ -51,6 +51,84 @@ export default function Example() {
           </div>
         </nav>
       </header>
+      <div className='m-auto my-10 px-4 max-w-[600px]'>
+        <form className="space-y-6" action="#" method="POST">
+          <div>
+            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+              Email
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
+                Nombre
+              </label>
+            </div>
+            <div className="mt-2">
+              <input
+                id="name"
+                name="name"
+                type="name"
+                autoComplete="current-name"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <label htmlFor="subject" className="block text-sm font-medium leading-6 text-gray-900">
+              Asunto
+            </label>
+            <div className="mt-2">
+              <input
+                id="subject"
+                name="subject"
+                type="subject"
+                autoComplete="subject"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <label htmlFor="message" className="block text-sm font-medium leading-6 text-gray-900">
+              Mensaje
+            </label>
+            <div className="mt-2">
+              <input
+                id="message"
+                name="message"
+                type="message"
+                autoComplete="message"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Consultar
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
