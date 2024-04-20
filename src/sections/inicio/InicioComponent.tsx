fa037a44b65fa606f0ea25ffb6b7d222178a8617
Juliana Battion <julibattion@gmail.com>
2024-04-20 18:11:59 -0300

    add new sections

---
+++ src/sections/inicio/InicioComponent.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from './inicio.module.css';
+
+export default function InicioComponent() {
+    return (
+        <div>
+            <section className={styles.index} id="inicio">
+        <div>
+        </div>
+            </section>
+        </div>
+    );
+}
