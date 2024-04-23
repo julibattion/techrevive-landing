e6cc9ff10a52bc4ece75a311b96f5f4341a87d42
Rodrigo Crespo <cresporodrigomanuel@gmail.com>
2024-04-22 22:53:55 -0300

    Fix footer

--- src/sections/footer/FooterComponent.tsx
+++ src/sections/footer/FooterComponent.tsx
@@ -5,16 +5,25 @@ import { Card, CardBody, CardHeader, Divider, Image } from "@nextui-org/react";
 export default function FooterComponent() {
     return (
         <footer className={`${styles.footer} bg-secondary`}>
-            <div className={styles.techFooter}>
-                <Image src="techrevive.png" alt="logo empresa" className={styles.techrevive} radius='none' />
+            <div className={styles.container}>
+                <div className={styles.techFooter}>
+                    <Image src="techrevive.png" alt="logo empresa" className={styles.techrevive} radius='none' />
+                </div>
+                <div className={styles.info}>
+                    <p>Paraguay 2215
+                    </p>
+                    <p>Cel. 3415024695</p>
+                    <p>techrevive.informatica@gmail.com</p>
+                </div>
+                <div className={styles.info}>
+                    <a className={styles.redes} href="https://www.instagram.com/techrevive.service/" target="_blank">
+                        <p>@techrevive.service</p>
+                        <Image width={50} height={50} src="instagram.png" alt="logo instagram" />
+                    </a>
+                </div>
             </div>
-            <hr />
-            <div className={styles.info}>
-                <p>Paraguay 2215
-                </p>
-                <p>3415024695</p>
-                <p>techrevive.informatica@gmail.com</p>
-                <div className={styles.redes}><a href="https://www.instagram.com/techrevive.service/" target="_blank"><Image src="instagram.png" alt="logo instagram" /></a><p>@techrevive.service</p></div>
+            <div className={styles.copyright}>
+                <p>© 2024 · Techrevive</p>
             </div>
         </footer>
     );
